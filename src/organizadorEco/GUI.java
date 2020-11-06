0576503cc433e4b44915872c55b49594a89c4593
AlberoArfax <alberoarfax7175@gmail.com>
2020-11-06 17:59:51 -0600

    Agregando comentarios de explicacion

--- src/organizadorEco/GUI.java
+++ src/organizadorEco/GUI.java
@@ -10,6 +10,7 @@ public class GUI {
 
     public GUI() {
 
+    	//Rectangulo de la aplicación
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(350, 600);
@@ -18,11 +19,13 @@ public class GUI {
         frame.setLayout(new BorderLayout(10, 10));
         frame.setLocationRelativeTo(null);
 
+        //Recuadro que contiene el titulo
         JPanel header = new JPanel();
         header.setBackground(new Color(0xB0FFA3));
         header.setPreferredSize(new Dimension(350, 60));
         header.setLayout(new BorderLayout(10, 10));
 
+        //Texto del titulo
         JLabel titulo = new JLabel(tituloStr);
         titulo.setFont(new Font("Montserrat", Font.PLAIN, 20));
         titulo.setVerticalAlignment(JLabel.CENTER);
@@ -31,11 +34,13 @@ public class GUI {
 
         header.add(titulo);
 
+        //Recuadro que contiene los botones en la parte de abajo
         JPanel footer = new JPanel();
         footer.setBackground(new Color(0x7EF36B));
         footer.setPreferredSize(new Dimension(350, 60));
         footer.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 15));
 
+        //Creacion de los "Botones"
         JLabel hechos = new JLabel();
         hechos.setIcon(new ImageIcon("imagenes/checkbox.png"));
         JLabel calendario = new JLabel();
@@ -49,10 +54,12 @@ public class GUI {
 
         JLabel[] imagenes = {hechos, calendario, home, basura, config};
 
+        //Adicion de los botones al footer
         for (JLabel imagen : imagenes) {
             footer.add(imagen);
         }
 
+        //Adicion de los elementos al frame
         frame.add(header, BorderLayout.NORTH);
         frame.add(footer, BorderLayout.SOUTH);
         frame.setVisible(true);
