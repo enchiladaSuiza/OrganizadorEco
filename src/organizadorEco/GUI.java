6c402ad99cd8fe48ec4492a31322853e056577ac
Santiago Vergara <santiagovrlr@gmail.com>
2020-11-07 16:11:36 -0600

    Bosquejo de la pantalla principal

--- src/organizadorEco/GUI.java
+++ src/organizadorEco/GUI.java
@@ -7,42 +7,52 @@ import java.awt.event.ActionListener;
 
 public class GUI {
 
+    JFrame frame;
+    JPanel header;
+    String tituloStr;
+    JLabel titulo;
+    JPanel footer;
+    JButton hechos;
+    JButton calendario;
+    JButton home;
+    JButton basura;
+    JButton config;
+    JPanel principal;
 
     public GUI() {
 
     	//Rectangulo de la aplicación
-        JFrame frame = new JFrame();
+        frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(350, 600);
-        frame.setTitle("Organizador EcolÃ³gico");
+        frame.setTitle("Organizador");
         frame.getContentPane().setBackground(new Color(0x27AE60));
-        frame.setLayout(new BorderLayout(10, 10));
+        frame.setLayout(new BorderLayout(0, 0));
         frame.setLocationRelativeTo(null);
 
         //Recuadro que contiene el titulo
-        JPanel header = new JPanel();
+        header = new JPanel();
         header.setBackground(new Color(0xB0FFA3));
         header.setPreferredSize(new Dimension(350, 60));
-        header.setLayout(new BorderLayout(10, 10));
+        header.setLayout(new BorderLayout(0, 0));
 
         //Texto del titulo
-        String tituloStr = "Just do that.";
-        JLabel titulo = new JLabel(tituloStr);
-        titulo.setFont(new Font("Montserrat", Font.PLAIN, 20));
+        tituloStr = "Just do that.";
+        titulo = new JLabel(tituloStr);
+        titulo.setFont(new Font("Montserrat", Font.BOLD, 20));
         titulo.setVerticalAlignment(JLabel.CENTER);
         titulo.setHorizontalAlignment(JLabel.CENTER);
         titulo.setForeground(Color.white);
-
         header.add(titulo);
 
         //Recuadro que contiene los botones en la parte de abajo
-        JPanel footer = new JPanel();
+        footer = new JPanel();
         footer.setBackground(new Color(0x7EF36B));
         footer.setPreferredSize(new Dimension(350, 60));
         footer.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 15));
 
-        //Creacion de los "Botones"
-        JButton hechos = new JButton();
+        //Creación de los "Botones"
+        hechos = new JButton();
         hechos.setIcon(new ImageIcon("imagenes/checkbox.png"));
         hechos.setBackground(null);
         hechos.setBorder(null);
@@ -53,7 +63,7 @@ public class GUI {
         	}
         });
         
-        JButton calendario = new JButton();
+        calendario = new JButton();
         calendario.setIcon(new ImageIcon("imagenes/calendar.png"));
         calendario.setBackground(null);
         calendario.setBorder(null);
@@ -64,7 +74,7 @@ public class GUI {
         	}
         });
         
-        JButton home = new JButton();
+        home = new JButton();
         home.setIcon(new ImageIcon("imagenes/home.png"));
         home.setBackground(null);
         home.setBorder(null);
@@ -75,7 +85,7 @@ public class GUI {
         	}
         });
         
-        JButton basura = new JButton();
+        basura = new JButton();
         basura.setIcon(new ImageIcon("imagenes/trash.png"));
         basura.setBackground(null);
         basura.setBorder(null);
@@ -86,7 +96,7 @@ public class GUI {
         	}
         });
         
-        JButton config = new JButton();
+        config = new JButton();
         config.setIcon(new ImageIcon("imagenes/gear.png"));
         config.setBackground(null);
         config.setBorder(null);
@@ -105,20 +115,30 @@ public class GUI {
         }
 
         //Panel Principal
-        JPanel principal = new JPanel();
-        principal.setBackground(Color.YELLOW);
-        principal.setPreferredSize(new Dimension(350, 480));
-        principal.setLayout(new FlowLayout(FlowLayout.CENTER));
+        principal = new JPanel();
+        principal.setBackground(new Color(0x27AE60));
+        principal.setPreferredSize(new Dimension(300, 100));
+        principal.setLayout(new CardLayout());
 
         //Panel Tasks
         JPanel task = new JPanel();
-        task.setBackground(new Color(0, 194, 0));
-        task.setPreferredSize(new Dimension(300, 80));
-        task.setLayout(new BorderLayout());
+        task.setBackground(new Color(0x27AE6A));
+        task.setSize(new Dimension(200, 100));
+        task.setOpaque(false);
+        task.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
+
+        JPanel[] pendientes = new JPanel[2];
+        for (JPanel pendiente : pendientes) {
+            pendiente = new JPanel();
+            // El tamaño debería ponerse según el tamaño del título, o algo así.
+            pendiente.setPreferredSize(new Dimension(300, 100));
+            pendiente.setBackground(new Color(0xAFF478));
+            task.add(pendiente);
+        }
 
         //Add Button
         JButton addOne = new JButton();
-        addOne.setIcon(new ImageIcon("images/anadir.png"));
+        addOne.setIcon(new ImageIcon("imagenes/plus.png"));
         addOne.setBackground(null);
         addOne.setBorder(null);
 
@@ -126,9 +146,13 @@ public class GUI {
             System.out.println("Agregando Task...");
             principal.add(task);
         });
-        principal.add(addOne);
+        //principal.add(addOne);
+        task.add(addOne);
 
-        //Adicion de los elementos al frame
+        // Adición de las "cartas" pricipal
+        principal.add(task);
+
+        //Adición de los elementos al frame
         frame.add(header, BorderLayout.NORTH);
         frame.add(footer, BorderLayout.SOUTH);
         frame.add(principal, BorderLayout.CENTER);
